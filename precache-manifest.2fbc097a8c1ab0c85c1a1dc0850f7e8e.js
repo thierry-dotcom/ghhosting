a26145c2e650583847374bc3486b0a0fbdac3b93
thierry-dotcom <t.kochuyt@gmail.com>
2019-11-29 15:25:09 +0100

    Updates

---
+++ precache-manifest.2fbc097a8c1ab0c85c1a1dc0850f7e8e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58943bf5f49d5b87a6b2caf098742709",
+    "url": "/ghhosting/index.html"
+  },
+  {
+    "revision": "9c5c27fa660c6e7eb29e",
+    "url": "/ghhosting/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "3d933fc47a46f1d2ec0a",
+    "url": "/ghhosting/static/js/2.97ff2a00.chunk.js"
+  },
+  {
+    "revision": "9c5c27fa660c6e7eb29e",
+    "url": "/ghhosting/static/js/main.5d2c8ebc.chunk.js"
+  },
+  {
+    "revision": "59e07949274ac53ae193",
+    "url": "/ghhosting/static/js/runtime-main.2fe3390f.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/ghhosting/static/media/logo.25bf045c.svg"
+  }
+]);
